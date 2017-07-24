taspelund/frr

pimd: Add 'debug pim trace detail' command
Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

pimd/pim_cmd.c
@@ -6076,6 +6076,17 @@ DEFUN (debug_pim_trace,
   return CMD_SUCCESS;
 }
 
+DEFUN (debug_pim_trace_detail,
+       debug_pim_trace_detail_cmd,
+       "debug pim trace detail",
+       DEBUG_STR
+       DEBUG_PIM_STR
+       DEBUG_PIM_TRACE_STR)
+{
+  PIM_DO_DEBUG_PIM_TRACE_DETAIL;
+  return CMD_SUCCESS;
+}
+
 DEFUN (no_debug_pim_trace,
        no_debug_pim_trace_cmd,
        "no debug pim trace",
@@ -6085,6 +6096,18 @@ DEFUN (no_debug_pim_trace,
        DEBUG_PIM_TRACE_STR)
 {
   PIM_DONT_DEBUG_PIM_TRACE;
+  return CMD_SUCCESS;
+}
+
+DEFUN (no_debug_pim_trace_detail,
+       no_debug_pim_trace_detail_cmd,
+       "no debug pim trace",
+       NO_STR
+       DEBUG_STR
+       DEBUG_PIM_STR
+       DEBUG_PIM_TRACE_STR)
+{
+  PIM_DONT_DEBUG_PIM_TRACE_DETAIL;
   return CMD_SUCCESS;
 }
 
@@ -7362,6 +7385,8 @@ void pim_cmd_init (void)
   install_element (ENABLE_NODE, &no_debug_pim_packetdump_recv_cmd);
   install_element (ENABLE_NODE, &debug_pim_trace_cmd);
   install_element (ENABLE_NODE, &no_debug_pim_trace_cmd);
+  install_element (ENABLE_NODE, &debug_pim_trace_detail_cmd);
+  install_element (ENABLE_NODE, &no_debug_pim_trace_detail_cmd);
   install_element (ENABLE_NODE, &debug_ssmpingd_cmd);
   install_element (ENABLE_NODE, &no_debug_ssmpingd_cmd);
   install_element (ENABLE_NODE, &debug_pim_zebra_cmd);
@@ -7398,6 +7423,8 @@ void pim_cmd_init (void)
   install_element (CONFIG_NODE, &no_debug_pim_packets_cmd);
   install_element (CONFIG_NODE, &debug_pim_trace_cmd);
   install_element (CONFIG_NODE, &no_debug_pim_trace_cmd);
+  install_element (CONFIG_NODE, &debug_pim_trace_detail_cmd);
+  install_element (CONFIG_NODE, &no_debug_pim_trace_detail_cmd);
   install_element (CONFIG_NODE, &debug_ssmpingd_cmd);
   install_element (CONFIG_NODE, &no_debug_ssmpingd_cmd);
   install_element (CONFIG_NODE, &debug_pim_zebra_cmd);

MrMandu/qpimd

pim_cmd.c
@@ -6503,6 +6503,17 @@ DEFUN (debug_pim_trace,
 	return CMD_SUCCESS;
 }
 
+DEFUN (debug_pim_trace_detail,
+       debug_pim_trace_detail_cmd,
+       "debug pim trace detail",
+       DEBUG_STR
+       DEBUG_PIM_STR
+       DEBUG_PIM_TRACE_STR)
+{
+	PIM_DO_DEBUG_PIM_TRACE_DETAIL;
+	return CMD_SUCCESS;
+}
+
 DEFUN (no_debug_pim_trace,
        no_debug_pim_trace_cmd,
        "no debug pim trace",
@@ -6512,6 +6523,18 @@ DEFUN (no_debug_pim_trace,
        DEBUG_PIM_TRACE_STR)
 {
 	PIM_DONT_DEBUG_PIM_TRACE;
+	return CMD_SUCCESS;
+}
+
+DEFUN (no_debug_pim_trace_detail,
+       no_debug_pim_trace_detail_cmd,
+       "no debug pim trace",
+       NO_STR
+       DEBUG_STR
+       DEBUG_PIM_STR
+       DEBUG_PIM_TRACE_STR)
+{
+	PIM_DONT_DEBUG_PIM_TRACE_DETAIL;
 	return CMD_SUCCESS;
 }
 
@@ -7817,6 +7840,8 @@ void pim_cmd_init(void)
 	install_element(ENABLE_NODE, &no_debug_pim_packetdump_recv_cmd);
 	install_element(ENABLE_NODE, &debug_pim_trace_cmd);
 	install_element(ENABLE_NODE, &no_debug_pim_trace_cmd);
+	install_element(ENABLE_NODE, &debug_pim_trace_detail_cmd);
+	install_element(ENABLE_NODE, &no_debug_pim_trace_detail_cmd);
 	install_element(ENABLE_NODE, &debug_ssmpingd_cmd);
 	install_element(ENABLE_NODE, &no_debug_ssmpingd_cmd);
 	install_element(ENABLE_NODE, &debug_pim_zebra_cmd);
@@ -7853,6 +7878,8 @@ void pim_cmd_init(void)
 	install_element(CONFIG_NODE, &no_debug_pim_packets_cmd);
 	install_element(CONFIG_NODE, &debug_pim_trace_cmd);
 	install_element(CONFIG_NODE, &no_debug_pim_trace_cmd);
+	install_element(CONFIG_NODE, &debug_pim_trace_detail_cmd);
+	install_element(CONFIG_NODE, &no_debug_pim_trace_detail_cmd);
 	install_element(CONFIG_NODE, &debug_ssmpingd_cmd);
 	install_element(CONFIG_NODE, &no_debug_ssmpingd_cmd);
 	install_element(CONFIG_NODE, &debug_pim_zebra_cmd);