taspelund/frr

pimd: pim_rp.c -> convert pimg to pim
Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

pimd/pim_neighbor.c
@@ -569,9 +569,9 @@ struct pim_neighbor *pim_neighbor_add(struct interface *ifp,
      Upon PIM neighbor UP, iterate all RPs and update
      nexthop cache with this neighbor.
    */
-  pim_resolve_rp_nh ();
+  pim_resolve_rp_nh (pim_ifp->pim);
 
-  pim_rp_setup ();
+  pim_rp_setup (pim_ifp->pim);
 
   pim_neighbor_rpf_update();
   return neigh;

MrMandu/qpimd

pim_neighbor.c
@@ -553,9 +553,9 @@ pim_neighbor_add(struct interface *ifp, struct in_addr source_addr,
 	   Upon PIM neighbor UP, iterate all RPs and update
 	   nexthop cache with this neighbor.
 	 */
-	pim_resolve_rp_nh();
+	pim_resolve_rp_nh(pim_ifp->pim);
 
-	pim_rp_setup();
+	pim_rp_setup(pim_ifp->pim);
 
 	pim_neighbor_rpf_update();
 	return neigh;